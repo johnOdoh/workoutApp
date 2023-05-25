3d7531850ed3546ede27f1fe07ccc15860863fd4
Johnny <chineduodoh4@gmail.com>
2023-05-25 12:23:26 +0100

    concluded the ExerciseCompletedActivity.kt nad its xml

--- app/src/main/java/com/example/workoutapp/ExerciseCompletedActivity.kt
+++ app/src/main/java/com/example/workoutapp/ExerciseCompletedActivity.kt
@@ -1,11 +1,26 @@
 package com.example.workoutapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.workoutapp.databinding.ActivityExerciseCompletedBinding
 
 class ExerciseCompletedActivity : AppCompatActivity() {
+    var binding: ActivityExerciseCompletedBinding? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_exercise_completed)
+        binding = ActivityExerciseCompletedBinding.inflate(layoutInflater)
+        setContentView(binding?.root)
+
+        binding?.btnHome?.setOnClickListener{
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        binding = null
     }
 }
